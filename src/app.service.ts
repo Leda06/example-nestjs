80c886f1b5bf08c603b07ce44179905e8c1de5e4
David Lechevalier <Leda06@users.noreply.github.com>
2024-05-14 12:23:21 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,6 +5,6 @@ const envToDisplay = process.env.ENV_TO_DISPLAY || 'nada' ;
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! This is the env to display ' + envToDisplay;
+    return `Hello World! This is the env to display ${envToDisplay}`;
   }
 }
