93a44fe7b7a29b04f741de34289d8b9650d228c6
David Lechevalier <Leda06@users.noreply.github.com>
2024-05-21 13:45:15 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 3000;
-console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`);
+const envToDisplay = process.env.ENV_TO_DISPLAY || 'nada' ;
+console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}, ENV: ${envToDisplay}`);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
