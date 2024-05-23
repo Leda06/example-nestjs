32f8feddf46de95e609147f6831fee34ab4cb829
David Lechevalier <Leda06@users.noreply.github.com>
2024-05-23 18:06:42 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,6 @@ const envToDisplay = process.env.ENV_TO_DISPLAY || 'nada' ;
 export class AppService {
   getHello(): string {
     console.log(`ENV: ${envToDisplay}`);
-    return `Hello World! This is the env to display ${envToDisplay}`;
+    return `Hello World! This is THE env to display ${envToDisplay}`;
   }
 }
